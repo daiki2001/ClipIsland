ddf9b2d6128f80faa9ff2fb2dad8d05154ccd2b3
YusukeNara <rakyyongaming@gmail.com>
2021-12-08 11:48:06 +0900

    テストでcppを追加、気にしなくて良い

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+//144396de8832c864fcdd6a01dd69254ddad2a621
+
+int main() {
+
+	cout << "hello world" << endl;
+
+	cout << "ny name is yusuke" << endl;
+
+	cout << "I haven't studied DirectX12" << endl;
+
+	return 0;
+}
