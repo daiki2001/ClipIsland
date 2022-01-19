0bd33129df8281bb239ef9c1dcf022d33b39fd60
エグチダイキ <74522674+daiki2001@users.noreply.github.com>
2022-01-20 02:23:50 +0900

    タイトルシーンの追加

--- main.cpp
+++ main.cpp
@@ -18,6 +18,14 @@ using namespace  BlockData;
 
 //-----------RakiEngine_Alpha.ver-----------//
 
+enum Scene
+{
+    TITLE,
+    SELECT,
+    GAME_MAIN,
+    GAME_OVER,
+    GAME_CLEAR
+};
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
@@ -55,12 +63,20 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
     Sprite background;
     background.CreateSprite({ (float)Raki_WinAPI::window_width, (float)Raki_WinAPI::window_height }, { 0.0f, 0.0f }, backgroundGraph, true);
 
+    /*タイトル*/
+    UINT titleGraph = TexManager::LoadTexture("./Resources/Title.png");
+    Sprite title;
+    title.CreateSprite({ (float)Raki_WinAPI::window_width, (float)Raki_WinAPI::window_height }, { 0.0f, 0.0f }, titleGraph, true);
+
     /*プレイヤー*/
     Player player;
 
     /*ステージ*/
     Stage stageData(&player);
     stageData.Select("test6.boxmap", true);
+
+    /*シーン遷移*/
+    Scene scene = Scene::TITLE;
 
     bool nFlag = false;
     bool actFlag = false;
@@ -74,146 +90,150 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 
         Input::StartGetInputState();
 
-        if (Input::isKey(DIK_1))
+        switch (scene)
         {
-            stageData.Select("map1.boxmap", true);
-            player.position = { 0.0f, -40.0f, 0.0f };
-        }
-        if (Input::isKey(DIK_2))
-        {
-            stageData.Select("map2.boxmap", true);
-            player.position = { 0.0f, -40.0f, 0.0f };
-        }
-        if (Input::isKey(DIK_3))
-        {
-            stageData.Select("map3.boxmap", true);
-            player.position = { 20.0f, -40.0f, 0.0f };
-        }
-        if (Input::isKey(DIK_4))
-        {
-            stageData.Select("test6.boxmap", true);
-        }
-        if (Input::isKey(DIK_5))
-        {
-            stageData.Select("test7.boxmap", true);
-        }
-        /*for (size_t i = 0; i < stageData.stage.collision.size(); i++)
-        {
-            bool AB = intersectAABB(player.playerCollision, stageData.stage.collision[i]);
-            if (AB == true)
+        case TITLE:
+            if (Input::isKey(DIK_SPACE))
             {
-                player.object->color = { 0, 0, 1, 1 };
-                if (stageData.stage.blockType[i] == BlockType::DONT_MOVE_BLOCK)
+                scene = Scene::SELECT;
+            }
+
+            // 描画開始
+            Raki_DX12B::Get()->StartDraw();
+
+            // スプライト描画
+            SpriteManager::Get()->SetCommonBeginDraw();
+            title.Draw();
+
+            // 描画終了
+            Raki_DX12B::Get()->EndDraw();
+            break;
+        case SELECT:
+            scene = Scene::GAME_MAIN;
+            break;
+        case GAME_MAIN:
+            if (Input::isKey(DIK_1))
+            {
+                stageData.Select("map1.boxmap", true);
+                player.position = { 0.0f, -40.0f, 0.0f };
+            }
+            if (Input::isKey(DIK_2))
+            {
+                stageData.Select("map2.boxmap", true);
+                player.position = { 0.0f, -40.0f, 0.0f };
+            }
+            if (Input::isKey(DIK_3))
+            {
+                stageData.Select("map3.boxmap", true);
+                player.position = { 20.0f, -40.0f, 0.0f };
+            }
+            if (Input::isKey(DIK_4))
+            {
+                stageData.Select("test6.boxmap", true);
+            }
+            if (Input::isKey(DIK_5))
+            {
+                stageData.Select("test7.boxmap", true);
+            }
+
+            player.Update();
+            stageData.Update();
+
+            nFlag = false;
+            actFlag = false;
+            actnFlag = false;
+
+            player.object->color = { 1, 1, 1, 1 };
+
+            for (size_t i = 0; i < stageData.stage.blocks.size(); i++)
+            {
+                actFlag = true;
+                if (player.position == stageData.stage.debugBoxObj[i]->position)
                 {
-                    
-                   player.moveFlag = false;
+                    nFlag = true;
+                    actFlag = false;
+                    break;
                 }
-                else {
-                    player.moveFlag = true;
+            }
+            ///true
+            for (size_t i = 0; i < stageData.stage.blocks.size(); i++)
+            {
+                actnFlag = true;
+                bool AB = intersectAABB(player.playerCollision, stageData.stage.blocks[i].collision);
+
+                if (stageData.stage.blocks[i].type == BlockType::GOAL && AB == true)
+                {
+                    player.goalFlag = true;
+                    player.object->color = { 0, 0, 1, 1 };
+
                 }
-                break;
+
+                if (AB == true)
+                {
+                    nFlag = stageData.stage.blocks[i].type == BlockType::DONT_MOVE_BLOCK || stageData.stage.blocks[i].type == BlockType::START || nFlag == false;
+                    actnFlag = false;
+                    break;
+                }
             }
-            else
+            ///false
+
+            if (nFlag == true)
             {
-                player.object->color = { 1, 1, 1, 1 };
+                player.position = player.playerOldPos;
+                player.object->position = player.position;
+                //player.object->color = { 0, 0, 1, 1 };
             }
-        }*/
 
-        player.Update();
-        stageData.Update();
-
-        nFlag = false;
-        actFlag = false;
-        actnFlag = false;
-
-        player.object->color = { 1, 1, 1, 1 };
-
-        for (size_t i = 0; i < stageData.stage.blocks.size(); i++)
-        {
-            actFlag = true;
-            if (player.position == stageData.stage.debugBoxObj[i]->position)
+            if (actFlag == true && actnFlag == true)
             {
-                nFlag = true;
-                actFlag = false;
-                break;
+                player.position = player.playerOldPos;
+                player.object->position = player.position;
+                //  player.object->color = { 0, 0, 1, 1 };
             }
-        }
-///true
-        for (size_t i = 0; i < stageData.stage.blocks.size(); i++)
-        {
-            actnFlag = true;
-            bool AB = intersectAABB(player.playerCollision, stageData.stage.blocks[i].collision);
-        
-            if (stageData.stage.blocks[i].type == BlockType::GOAL && AB == true)
+
+            // player.object->color = { 1, 1, 1, 1 };
+
+            stageData.Clip(Input::isKeyTrigger(DIK_SPACE));
+
+            if (Input::isKeyTrigger(DIK_R))
             {
-                player.goalFlag = true;
-                player.object->color = { 0, 0, 1, 1 };
-                
+                stageData.Reset();
             }
-        
-            if (AB == true)
+
+            if (Input::isKeyTrigger(DIK_B))
             {
-//<<<<<<< HEAD
-//                nFlag = stageData.stage.blockType[i] == BlockType::DONT_MOVE_BLOCK || stageData.stage.blockType[i] == BlockType::START || nFlag == false;
-//=======
-                nFlag = stageData.stage.blocks[i].type == BlockType::DONT_MOVE_BLOCK || nFlag == false;
-//>>>>>>> 2cdde459b20d42ed3b3fdb3bdccf65711041219a
-                actnFlag = false;
-                break;
+                stageData.StepBack();
             }
+
+            NY_Object3DManager::Get()->UpdateAllObjects();
+
+            // 描画開始
+            Raki_DX12B::Get()->StartDraw();
+
+            // 背景描画
+            SpriteManager::Get()->SetCommonBeginDraw();
+            background.Draw();
+
+            // オブジェクト描画
+            NY_Object3DManager::Get()->SetCommonBeginDrawObject3D();
+            player.Draw();
+            stageData.Draw();
+
+            // 前景描画
+            SpriteManager::Get()->SetCommonBeginDraw();
+
+            // 描画終了
+            Raki_DX12B::Get()->EndDraw();
+            break;
+        case GAME_OVER:
+            scene = Scene::TITLE;
+            break;
+        case GAME_CLEAR:
+            scene = Scene::TITLE;
+            break;
+        default:
+            break;
         }
-///false
-
-
-        if (nFlag == true)
-        {
-            player.position = player.playerOldPos;
-            player.object->position = player.position;
-            //player.object->color = { 0, 0, 1, 1 };
-        }
-
-        if (actFlag == true && actnFlag == true)
-        {
-            player.position = player.playerOldPos;
-            player.object->position = player.position;
-          //  player.object->color = { 0, 0, 1, 1 };
-        }
-
-
-       // player.object->color = { 1, 1, 1, 1 };
-
-
-        stageData.Clip(Input::isKeyTrigger(DIK_SPACE));
-
-        if (Input::isKeyTrigger(DIK_R))
-        {
-            stageData.Reset();
-        }
-
-        if (Input::isKeyTrigger(DIK_B))
-        {
-            stageData.StepBack();
-        }
-
-        NY_Object3DManager::Get()->UpdateAllObjects();
-
-        // 描画開始
-        Raki_DX12B::Get()->StartDraw();
-
-        // 背景描画
-        SpriteManager::Get()->SetCommonBeginDraw();
-        background.Draw();
-
-        // オブジェクト描画
-        NY_Object3DManager::Get()->SetCommonBeginDrawObject3D();
-        player.Draw();
-        stageData.Draw();
-
-        // 前景描画
-        SpriteManager::Get()->SetCommonBeginDraw();
-
-        // 描画終了
-        Raki_DX12B::Get()->EndDraw();
 
         if (Input::isKey(DIK_ESCAPE))
         {
